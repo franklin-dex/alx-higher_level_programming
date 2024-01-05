50f1f54111456f7314a187c31851feeb2c373844
Frankiln <odinakachukwufranklin0@gmail.com>
2024-01-04 16:32:11 -0800

    edit program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
-    from add_0 import add
+
     solution = add(a, b)
     print(f'[a] + [b] = [solution]')
