0e350e0547349e3165c0d059e275ff2d0fc0b6a8
Frankiln <odinakachukwufranklin0@gmail.com>
2024-01-04 17:52:37 -0800

    edit add function file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
+
     a = 1
     b = 2
-    print(f"[] + [] = []".format(a, b, add(a, b)))
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
