dc827e01663a728e5650f7d69ce872e852bb913f
Frankiln <odinakachukwufranklin0@gmail.com>
2024-03-19 01:55:05 -0700

    update function that raises a name exception with a message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def raise_exception_msg(message=""):
-    try:
-        raise NameError(message)
-    except NameError as e:
-        print(e)
-        raise
+    raise NameError(message)
+
+
+if __name__ == "__main__":
+    raise_exception_msg("C is fun")
