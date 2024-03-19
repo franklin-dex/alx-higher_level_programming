e1b3bf28b9889babb2836b48b67dce1f5bc3d51d
Frankiln <odinakachukwufranklin0@gmail.com>
2024-03-19 00:15:19 -0700

    add  a function that prints an integer with "{:d}".format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,9 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except ValueError:
         return False
+
 
 if __name__ == "__main__":
     value = 89
@@ -12,11 +13,13 @@ if __name__ == "__main__":
     if not has_been_print:
         print("{} is not an integer".format(value))
 
+    
     value = -89
     has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
 
+    
     value = "School"
     has_been_print = safe_print_integer(value)
     if not has_been_print:
