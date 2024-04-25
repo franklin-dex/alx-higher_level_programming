f76bfdd24874274d08a388650e910b480faa76c2
Frankiln <odinakachukwufranklin0@gmail.com>
2024-04-25 05:56:45 -0700

    add Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#this script takes in a url, send a GET request using curl, displays the body if the status code is 200
-curl -sL "$1" -o /tmp/body.txt && grep -F 200 /tmp/body.txt > /dev/null && cat /tmp/body.txt
+#this script takes in a url, send a GET request using curl, displays the body if status code is 200
+[ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1; curl -sL "$1" -o /tmp/body.txt; grep -q "200 OK" /tmp/body.txt && cat /tmp/body.txt; rm -f /tmp/body.txt
