a970fa9e115cfc925375fd9ce1cf05630b9994cc
Frankiln <odinakachukwufranklin0@gmail.com>
2024-05-04 21:43:27 -0700

    add function that multiplies 2 matrices by using the module NumPy

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
+"""
+Module add-integer
+Adds two integer together
+
+"""
+
 
 def add_integer(a, b=98):
+    """Returns the addition of a and b,
+    or an error if a and b are not integers or floats
     """
-    Adds two integers.
 
-    Args:
-    a (int or float): The first number.
-    b (int or float): The second number. Default is 98.
-
-    Returns:
-    int: The sum of a and b.
-
-    Raises:
-    TypeError: If a or b is not an integer or float.
-    """
-    if not isinstance(a, (int, float)):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
